4b66f5acad537b666c7872461943f8dcf179d739
Max <lxj970814@gmail.com>
2025-10-31 00:37:18 -0700

    update_file_a_bit

--- module5_call.py
+++ module5_call.py
@@ -1,26 +1,8 @@
-# 1. Create a Python program:
-
-# The program asks the user for input N (positive integer) and reads it
-
-# Then the program asks the user to provide N numbers (one by one) and reads all of them (again, one by one)
-
-# In the end, the program asks the user for input X (integer) and outputs: "-1" if there were no such X among N read numbers, or the index (from 1 to N) of this X if the user inputted it before.
-
-# The basic functionality of data processing (data initialization, data insertion, data search) should be done via Object-Oriented Programming Paradigm (i.e. using Classes)
-
-# Name this program "module5_oop.py" and upload it to the above-mentioned repo.
-
-# 2. Create the functionality outside of the main running code: i.e., create the module with the Class described in the item above, and name this module "module5_mod.py" + create the main program "module5_call.py" that uses the Class description from the "*_mod" file.  Upload everything to the above-mentioned repo.
-
 import module5_mod
 
 def main():
-    module5_mod.number_processor.initialize_n()
-
-    module5_mod.number_processor.initialize_numbers(module5_mod.number_processor.number_size)
-
-    module5_mod.number_processor.find_number()
-
+    processor = module5_mod.NumberProcessor()
+    processor.process()
 
 if __name__ == "__main__":
     main()
